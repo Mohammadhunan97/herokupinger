08924b4c2c17893fd0a2bce385eba35452d6b922
= <=>
2017-07-26 02:05:22 -0400

    qw

--- public/script.js
+++ public/script.js
@@ -2,13 +2,19 @@ window.onload = function(){
 	console.log("dom loaded")
 	document.getElementsByTagName("body")[0].style.backgroundImage = "url('https://s-media-cache-ak0.pinimg.com/originals/2b/ee/6d/2bee6d511ed7cf38084283f7921eb30c.jpg')";
 
- if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js')
-      .then(function(reg){
-        console.log("Yes, it did.");
-      }).catch(function(err) {
-        console.log("No it didn't. This happened: ", err)
-      });
-  }
+window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/static/service-worker.js', {
+            scope: './static/'
+        })
+        .then(function (r) {
+            console.log('Registered Service Worker');
+        })
+        .catch(function (whut) {
+            console.error('Uh oh, there is a problem... ');
+            console.error(whut);
+        });
+    window.addEventListener('beforeinstallprompt', function (e) {
+       		console.log('beforeinstallprompt Event fired');
+    });
+});
 }
